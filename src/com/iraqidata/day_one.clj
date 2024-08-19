9f7221bf8b76b9535a486b290a619c7dc85c4ec7
adham-omran-ii <adham.a@kapita.iq>
2024-08-19 12:16:55 +0300

    Apply grading

--- src/com/iraqidata/day_one.clj
+++ src/com/iraqidata/day_one.clj
@@ -1,11 +1,23 @@
 (ns com.iraqidata.day-one)
 
-
+;; Issues:
+;; 1. The branch was not checked out from the correct branch.
+;; 2. Too many files were committed at once. (1501 files changed, 1641 insertions(+)).
 
 ;;1
-(defn pr [arg]
-  arg)
-(pr 4)
+;; (defn pr [arg]
+;;   arg)
+;; (pr 4)
+
+(defn my-pr [arg]
+  arg )
+;; Kindly check the warning provided by VSCode for the naming of the function,
+;; `pr` is a built in function.
+
+(my-pr 4)
+
+;; Incorrect, the function returns `4` rather than `nil` and does not print any
+;; value.
 
 ;;---------------------------------------------------
 
@@ -16,6 +28,7 @@
     :error))
 (add 1 2)
 
+;; Correct, one full point.
 
 ;;----------------------------------------------------
 
@@ -39,8 +52,15 @@
           (- year born))})
 (:age my-map)
 
+;; The tasks specified a function to be made rather than a map.
+
+;; Incorrect.
+
 ;;------------------------------------------------------
 ;;4
 
 (if (>= (:age my-map) 18) true false)
 
+;; Same as 3, the tasks specified a function to be made rather than an call.
+
+;; Total: 1 point.
